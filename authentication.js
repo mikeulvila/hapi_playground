4cc5d00f676fa1eb66a3260dfe9ed1cee51ebb36
Mike Ulvila <mikeulvila@me.com>
2016-07-23 22:46:33 -0500

    validate function using Bcrypt to compare password

--- authentication.js
+++ authentication.js
@@ -11,10 +11,25 @@ server.connection({
 });
 
 const users = {
+
   john: {
     username: 'john',
     password: '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm', // 'secret'
     name: 'John Doe',
     id: '2133d32a'
   }
+
+};
+
+const validate = function(request, username, password, callback) {
+
+  const user = users[username];
+  if (!user) {
+    return callback(null, false);
+  }
+
+  Bcrypt.compare(password, user.password, (err, isValid) => {
+    callback(err, isValid, { id: user.id, name: user.name });
+  });
+
 };
